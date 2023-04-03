555580a299224f1f195bd751d48d045ebe66fd25
Nafis.3000 <nafis.3000@gmail.com>
2023-04-03 15:53:49 -0600

    fix: http-equiv

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,16 @@ import Head from "next/head";
 import Link from "next/link";
 import { Weather } from "../components/Weather";
 
-
-
 const Home = () => {
   return (
     <>
+      <Head>
+        <title>Home</title>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        ></meta>
+      </Head>
       <Weather />
     </>
   );
